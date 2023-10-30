d4f8fe92bc9e28e542af45e65fed915780b252d6
Farouk Altahesh <faroukaltahesh2004@gmail.com>
2023-10-30 09:33:29 -0400

    Started to name charachters, and setting up the basics of my chapter.

--- CLIStoryTemplate/Chapter 3/Chapter3.swift
+++ CLIStoryTemplate/Chapter 3/Chapter3.swift
@@ -6,8 +6,40 @@
 import Foundation
 
 func chapterThree() {
-    // Once the information and secrets unravel, more victims and a society become known
-
-    let nameOfCharacter 
     
-}
+    // chapter 3 is the serial killers pov.
+    // plot twist: the killer isnt actually alive for 150 years. He actaully died in 1975, the world was just so fascinated by his sheer intelligence that because they kept talking about it kept him alive.
+    
+
+        let nameOfKiller = "Man-ripulator"
+        let nameOfVitcim1 = "Mateo"
+        let nameOfVitcim2 = "Rufus"
+        let nameOfVitcim3 = "Angelina"
+        var dateOfDay = 10/5/1975
+        var settingOfStory = "Canada"
+        var ageOfVictims = [12, 23, 45, 76, 10, 20, 32]
+        
+        
+        
+        
+        print("In a place where you think lives an innocent child, where playboy posters hang lifelessly and random clothes scattered on the ground. ")
+        sleep(2)
+        
+        print("Pretty normal... or so you think... ")
+        sleep(2)
+        
+        print("Mice ran through the walls, and critters freely inhabitted the ground below. ")
+        
+        
+    }
+        
+    
+    
+    
+    
+
+    
+    
+    
+    
+

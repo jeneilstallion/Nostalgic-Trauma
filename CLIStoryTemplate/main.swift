a402ecb348bfa70fcdf8553af15a3f6a48762d91
Jeneil Stallion <jeneilstallion@gmail.com>
2023-11-01 09:17:46 -0400

    Ch 4. JS Update

---
+++ CLIStoryTemplate/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  Nostalgic Trauma
+//
+//  Created by Jeneil Stallion on 11/1/23.
+//
+
+import Foundation
+
+func readStory() {
+    chapterOne()
+    chapterTwo()
+    chapterThree()
+    chapterFour()
+    chapterFive()
+}
+
+readStory()

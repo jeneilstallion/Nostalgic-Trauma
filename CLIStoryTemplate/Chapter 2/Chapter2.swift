27915aea0dd09afc6d0a9566dd466218e893b10f
Kirby <leiland3605@gmail.com>
2023-11-02 09:17:44 -0400

    ggg

--- CLIStoryTemplate/Chapter 2/Chapter2.swift
+++ CLIStoryTemplate/Chapter 2/Chapter2.swift
@@ -3,8 +3,79 @@
 //  CLIStoryTemplate
 //
 
-import Foundation
 
-func chapterTwo() {
-    // Your portion of the story goes here
-}
+    import Foundation
+
+    var age = 40
+    let prisonerName: String = "Kaden"
+    let nickNames: [String] = ["Dismembermented, The kazekage killer, Shadow Walker"]
+    let favNick: String = "variance"
+
+    func checkAge() {
+        if age == 40 {
+            print("I have been here for 15 GOD FORSAKEN YEARS AND IS STILL SERVING LIFE!!")
+        } else {
+            print("Screw you")
+        }
+    }
+
+    func chapterTwo() {
+        // Your portion of the story goes here
+        print("Chapter 2")
+            
+        print("The Visit")
+        
+        print("My name is \(prisonerName) but you may know me by \(nickNames) but my personal Favorite Is \(favNick) and I am \(age) years old and is currently serving my life sentence in prison.")
+        sleep(2)
+        print("One day as I was relaxing in my cold see though chamber surrounded by me keepers an old friend visited me.")
+        sleep(2)
+        print("Well isnt this a suprise, I said with malice i my voice. Detective Raven my captor.")
+        sleep(2)
+        print("To what do I owe the pleasure of this uninvited visit detective, I said with a sarcastic voice.")
+        sleep(2)
+        print("He said he needed my help and I began to become enraged.")
+        sleep(2)
+        print("\(checkAge()) AND YOU DARE TO COME IN HERE AND STILL ASK FOR MY HELP!!")
+        sleep(2)
+        print("YOUR THE ONE WHO PUT ME HERE!!")
+        sleep(2)
+        print("I said out of pure malice and started to realize the prison gaurds clutch on their weapons.")
+        sleep(2)
+        print("I took a breather and started to calm down but was still agitated.")
+        sleep(2)
+        print("I told him to leave and don't come back but he reluctantly declined.")
+        sleep(2)
+        print("He said there is a new serial killer and he needed to know how he thinks.")
+        sleep(2)
+        print("I said that sounded like a personal problem and had nothing to do with me.")
+        sleep(2)
+        print("I turned aroung and was going to ignore him and continue with my day but something he said caught my attention.")
+        sleep(2)
+        print("What did you say I asked. He said their calling him the new nightmare of the people and that I was old news.")
+        sleep(2)
+        print("Me old news I chuckled. The detective knew just how to get under my skin and use my pride against me.")
+        sleep(2)
+        print("Thats how they caught me, but Im different I thought to myself.")
+        sleep(2)
+        print("Alright detective you've peeked my interest it hurts my pride to be forgotten and replaced by a lesser version though I wont help without something in return.")
+        sleep(2)
+        print("In exchange for helping I want my freedom.")
+        sleep(2)
+        print("Im sick and tired of this place its no fun anymore really.")
+        sleep(2)
+        print("So what do you say detective how badley do you need my help.")
+        sleep(2)
+        print("I could see it on his face the detective was bewildered and with this fact alone I knew my freedom was approaching it just needed a little extra push.")
+        sleep(2)
+        print("Come on detective on killer for the price of another whose causing more trouble for the public eye.")
+        sleep(2)
+        print("I see that he was thinking immensely couldn't have that now can we.")
+        sleep(2)
+        print("Did I also forget to mension the offer is off the table after 20 seconds and you've waisted half.")
+        sleep(2)
+        print("Ten seconds left detective the clock is ticking")
+        for countDown in 10...1 {
+            print(countDown)
+        }
+        
+    }
--- CLIStoryTemplate/Chapter 3/Chapter3.swift
+++ CLIStoryTemplate/Chapter 3/Chapter3.swift
@@ -8,6 +8,6 @@ import Foundation
 func chapterThree() {
     // Once the information and secrets unravel, more victims and a society become known
 
-    let nameOfCharacter 
+   // let nameOfCharacter 
     
 }
